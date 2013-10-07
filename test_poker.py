8f73499cb3a1d9648fe23296646f7f25876974ff
luciferes <s6070080@kmitl.ac.th>
2013-10-07 11:58:56 +0700

    put the tests in test_poker.py

---
+++ test_poker.py
@@ -0,0 +1,29 @@
+import unittest
+import poker
+ 
+class TestPoker(unittest.TestCase):
+    '''Example unittest test methods for poker.'''
+    sf = ['JC', 'TC', '9C', '8C', '7C']
+    fk = ['5S', '5H', '5D', '5C', 'KS']
+    fh = ['5S', '5H', '5D', '8C', '8S']
+    s1 = ['JC', 'TC', '9C', '8S', '7C']
+    op = ['5S', '3H', '9D', '8C', '8S']
+    tp = ['5S', '5H', '9D', '8C', '8S']
+    hc = ['4S', '3H', '9D', '8C', 'TS']
+    
+    def test_poker_example_1(self):
+        '''Test get poker with sf(straight flush) and fk(4 kinds).'''
+ 
+        actual = poker.poker([TestPoker.sf,TestPoker.fk])
+        expected = ['JC', 'TC', '9C', '8C', '7C']
+        self.assertEqual(actual, expected)
+ 
+    def test_poker_example_2(self):
+        '''Test get divisors with fh(full house) and fk(4 kinds).'''
+ 
+        actual = poker.poker([TestPoker.fh,TestPoker.fk])
+        expected = ['5S', '5H', '5D', '5C', 'KS']
+        self.assertEqual(actual, expected)
+ 
+if __name__ == '__main__':
+    unittest.main(exit=False)
